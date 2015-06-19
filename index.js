b2a7374674d84073f4a24280a387e787dd8efdd8
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2015-06-19 23:31:05 +0000

    now compatible with port 8080 and morgan module

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express')
 var fs = require('fs')
+var logger = require('morgan')
+
 var app = express()
+app.use(logger('common', {}))
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var index = "index.html"
-  var fileBuffer = fs.readFileSync(index)
-  response.send(fileBuffer.toString())
+  var indexFile = "index.html"
+  var html= fs.readFileSync(indexFile).toString()
+  response.send(html)
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 8080
+app.listen(port, function() {
+  console.log("Listening on " + port)
 })
